eff89489461dc102f4f351e45077fb45930c0b41
aditya-0902 <88245845+aditya-0902@users.noreply.github.com>
2021-08-18 15:24:09 +0530

    solved

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,24 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+		this.state={
+			phidden:true
+		}
 	};
 
     render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
+				<button id="click" onClick={
+					()=>{
+						this.setState({
+							phidden: !this.state.phidden
+						})
+					}
+				}>show text</button>
+				{this.state.phidden ? null : <p id="para">
+					Hello, I've learnt to use the full-stack evaluation tool. 
+					This makes me so happy</p>}
     		</div>
     	);
     }
